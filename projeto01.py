2134a922346a1fb68e9c06473ab89079be0930f2
Thiago - IBM <IBM@MacBook-Pro-de-Thiago.local>
2023-01-25 11:23:10 -0300

    first commit

---
+++ projeto01.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city = input('Qual o nome da cidade em que você nasceu?\n')
+pet = input('Qual o nome do seu pet?\n')
+print(f'O nome da sua banda pode ser: {city + " " + pet}!')
